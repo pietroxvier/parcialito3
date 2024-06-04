0996790d2560d06a2a0493491b07ec1ecd490a6f
pietroxvier <pietroxvier@gmail.com>
2024-06-04 20:56:01 -0300

    Update cadastro.js

--- public/script/cadastro.js
+++ public/script/cadastro.js
@@ -8,7 +8,7 @@ document.getElementById('registration-form').addEventListener('submit', function
     const password = document.getElementById('password').value;
   
     // Padrão do url
-    const serverUrl = `${window.location.protocol}//${window.location.hostname}`;
+    const serverUrl = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
     const registerUrl = `${serverUrl}/cadastro`;
     // Constrói o objeto de dados a ser enviado para a API
     const data = {
