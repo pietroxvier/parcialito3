9a305126f2edbf7a2bfeeae8622dd25f4a4d4ad2
pietroxvier <pietroxvier@gmail.com>
2024-06-09 22:37:59 -0300

    Update script2.js

--- public/script/script2.js
+++ public/script/script2.js
@@ -255,7 +255,7 @@ new Vue({
     this.loadUserData();
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        navigator.serviceWorker.register('script/service-worker.js').then(registration => {
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }).catch(error => {
           console.log('ServiceWorker registration failed: ', error);
